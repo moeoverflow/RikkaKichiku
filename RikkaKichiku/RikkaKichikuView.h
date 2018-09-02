28f1ea503050b574eb136b15589b28c6381bb21c
ShinCurry <shincurryyang@gmail.com>
2018-09-02 13:26:13 +0800

    initial project

---
+++ RikkaKichiku/RikkaKichikuView.h
@@ -0,0 +1,13 @@
+//
+//  RikkaKichikuView.h
+//  RikkaKichiku
+//
+//  Created by ShinCurry on 2018/9/2.
+//  Copyright © 2018年 Moeoverflow. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface RikkaKichikuView : ScreenSaverView
+
+@end
